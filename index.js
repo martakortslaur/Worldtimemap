be0d7a884821caa7e9c2f09c8f6bc7a68edd28c4
martakortslaur <martakortslaur@gmail.com>
2019-11-11 15:10:07 +0000

    Setting up API, push

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var map;
-var zoom_level = 8;
+var uluru = {lat: -25.344, lng: 131.036};
+var zoom_level = 3;
 
 function initMap() {
 
-    map = new google.maps.Map(document.getElementById('map'), {zoom: zoom_level
-    });
+    map = new google.maps.Map(document.getElementById('map'),   {   center: uluru,
+                                                                    zoom: zoom_level,
+                                                                });
+    var marker_uluru = new google.maps.Marker({position: uluru, map: map});
 }
