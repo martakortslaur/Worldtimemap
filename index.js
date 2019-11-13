755ca7e451c60bd8cf18b3f39b339244f6bc2eaf
martakortslaur <martakortslaur@gmail.com>
2019-11-13 16:39:51 +0000

    Fixing buggs

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 function initMap() {
     var options = {
         zoom:8,
-        center:{lat:51.476852, lng: -0.000500}
+        center:{lat:51.476852, lng:-0.000500}
     }
+
     //New map
-    var map = new
-    google.maps.Map(document.getElementById('map'),  options);
-    
+    var map = new google.maps.Map(document.getElementById('map'),  options);
+
     //Listen for click on map
     google.maps.event.addListener(map, 'click',
     function(event){
         //Add marker
         addMarker({coords:event.latLng});
     });
-    // Add marker                                                               });
-    // var marker_greenwich = new google.maps.Marker({position: greenwich, map: map});
-
-    // var infoWindow = new google.maps.InfoWindow({
-    //     content: '<h1>Greenwich Royal Observatory</h1>'
-    // });
-
-    // marker.addListener('click', function(){
-    //     infoWindow.open(map, marker);
-    // });
+}
 
     // Array of markers
     var markers = [
         {
-        coords:{lat: 51.476852,lng: -0.000500},
-        iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        content:'<h1>Greenwich</p>'
+        coords:{lat:51.476852,lng:-0.000500},
+        iconImage:'http://maps.google.com/mapfiles/kml/pushpin/wht-pushpin.png';
+        content:'<h1>Greenwich</h1>'
         },
         {
         coords:{lat:49.6116,lng:6.1319},
+        iconImage:'http://maps.google.com/mapfiles/kml/pushpin/wht-pushpin.png',
         content:'<h1>Luxembourg</h1>'
+        },
+        {
+        coords:{lat:40.7128,lng:74.0060},
+        iconImage:'http://maps.google.com/mapfiles/kml/pushpin/wht-pushpin.png'
+        },
+        {
+        coords:{lat:35.6804,lng:139.7690},
+        iconImage:'http://maps.google.com/mapfiles/kml/pushpin/wht-pushpin.png'
         }
 
     ];
+
     //Loop through markers
-    for(var i = 0;i < markers.length;i++){
+    for(var i = 0; i < markers.length; i++){
         //Add marker
         addMarker(markers[i]);
     }
-    
-    // Add marker function
+
+   // Add marker function
     function addMarker(props){
         var marker = new google.maps.Marker({
         position:props.coords,
         map:map,
-        // icon: props.iconImage
-
     });
+    
     // Check for customicon
     if(props.iconImage){
         // Set icon image
@@ -66,7 +66,4 @@ function initMap() {
      marker.addListener('click', function(){
         infoWindow.open(map, marker);
     });
-}
-
-}
-}
+    }
